62bb0b209fbafebc2369a8ca6b6750c856af3afb
Lautaro Silverii (Notebook) <lautarosilverii@gmail.com>
2024-01-29 13:39:31 -0300

    name added to routes

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,17 @@ use App\Http\Controllers\WebController;
 /*************************************************************/
 
 //=====> GET
-    Route::get('/', [WebController::class, 'home']);
-    Route::get('/proveedores', [WebController::class, 'get_providers']);
-    Route::get('/equipos', [WebController::class, 'get_equipments']);
-    Route::get('/proveedores/{id}', [WebController::class, 'get_provider']);
-    Route::get('/equipos/{id}', [WebController::class, 'get_equipment']);
+    Route::get('/', [WebController::class, 'home'])->name('home');
+    Route::get('/proveedores', [WebController::class, 'get_providers'])->name('providers');
+    Route::get('/equipos', [WebController::class, 'get_equipments'])->name('equipments');
+    Route::get('/proveedores/{id}', [WebController::class, 'get_provider'])->name('provider');
+    Route::get('/equipos/{id}', [WebController::class, 'get_equipment'])->name('equipment');
+    Route::get('/backups', [WebController::class, 'get_backups'])->name('backups');
+    Route::get('/backups/{date}', [WebController::class, 'download_backup'])->name('backup');
 //=====> POST
 
 //=====> PUT
-    
-//=====> DELETE
+    Route::post('/backups', [WebController::class, 'add_backup']);
+    Route::get('/', [WebController::class, 'home']);
+//=====> DELTE
+    Route::delete('/backups/{date}', [WebController::class, 'delete_backup']);
