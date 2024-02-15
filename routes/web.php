82e483ffcb8c12faf6a8b43d97b170a3b57f3134
Lauty128 <lautarosilverii@gmail.com>
2024-02-15 15:00:07 -0300

    Laravel Breeze added

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,25 @@
 <?php
 
-//-----> Dependencies 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 //-----> Controllers
 use App\Http\Controllers\WebController;
 
 
 /*************************************************************/
-/******************** ROUTES *********************************/
+/******************** PUBLIC *********************************/
 /*************************************************************/
 
 //=====> GET
@@ -25,3 +36,19 @@ use App\Http\Controllers\WebController;
 
 //=====> DELTE
     Route::delete('/backups/{date}', [WebController::class, 'delete_backup']);
+
+    
+/*************************************************************/
+/******************** DASHBOARD *********************************/
+/*************************************************************/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
