778754036ef6eb537d78cbe1019db1604135aaa3
Lauty128 <lautarosilverii@gmail.com>
2024-03-22 10:54:58 -0300

    Controller in API added to search the equipments that doesnt has a specified provider

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 //----> Dependencies
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 //----> Models
 use App\Models\Equipment;
@@ -31,6 +32,27 @@ class ApiController extends Controller
             ->where('provider_equipment.equipment_id', $id) 
             ->paginate(10);
             
+        return response()->json($equipments);
+    }
+
+    public function equipments_that_doesnt_has_a_provider(Request $request, string $id)
+    {
+        $equipments = DB::table('equipments', 'e')
+            ->select(['e.id','e.name', 'e.category_id','categories.name AS category_name'])
+            ->join('categories', 'e.category_id', 'categories.id') // Join to get category name
+            ->leftJoin('provider_equipment', function ($join) use ($id) { // Left join to provider_equipment table
+                $join->on('e.id', '=', 'provider_equipment.equipment_id') // (LEFT == equipments.id)
+                     ->where('provider_equipment.provider_id', '=', $id); // Force relation with the provider $id
+            })
+            ->where(function($query) use($id){
+                $query->whereNull('provider_equipment.provider_id') // Where provider_id is null
+                    ->orWhere('provider_equipment.provider_id', '!=', $id); // Or Where provider_id is different to $id
+            });
+            
+        if($request->search) $equipments->where('e.name', 'like', '%'.$request->search.'%');
+            
+        $equipments = $equipments->paginate(20);
+
         return response()->json($equipments);
     }
 }
